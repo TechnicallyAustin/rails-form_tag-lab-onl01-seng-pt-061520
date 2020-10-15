1af6dd3ecbc6b7a8fe0d3c344ecb97a2b7d6562d
Technicallyaustin <Technicallyaustin@gmail.com>
2020-10-15 11:46:28 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+
 class StudentsController < ApplicationController
   
   def index
@@ -7,5 +8,22 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+
+  def new
+    #@student = Student.create(:first_name params(student[:first_name], :last_name params(student[:last_name]))
+    #redirect_to new_student_path(@student)
+  end
+
+  def create
+   # use nested hashes here to grab the parameters created from the new form 
+   # student[;first_name] and student[:last_name]
+
+   # create a new student object and redierct to the students#index
+   @student = Student.create(first_name: params[:first_name], last_name: params[:last_name] )
+   
+   redirect_to students_path(@student)
+  end
+
+
 
 end
